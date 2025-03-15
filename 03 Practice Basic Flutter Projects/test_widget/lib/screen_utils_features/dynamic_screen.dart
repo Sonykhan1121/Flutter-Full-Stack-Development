bcbf38034b66e9a90c57d9b93aa0e2e1dcae4dbe
sonykhan1121 <sonykhan1121@gmail.com>
2025-03-15 08:57:42 +0600

    Screen Util features

---
+++ 03 Practice Basic Flutter Projects/test_widget/lib/screen_utils_features/dynamic_screen.dart
@@ -0,0 +1,45 @@
+/*
+import 'package:attendence_ui/signUpandLoginUser_features/pages/navigation_page.dart';
+import 'package:attendence_ui/signUpandLoginUser_features/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+      ChangeNotifierProvider(create: (_)=>UserProvider()),
+    ],
+    child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(            <-------here is the screen util package
+      designSize: Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home:  NavigationPage(),
+        );
+      },
+
+    );
+  }
+}
+
+
+ */

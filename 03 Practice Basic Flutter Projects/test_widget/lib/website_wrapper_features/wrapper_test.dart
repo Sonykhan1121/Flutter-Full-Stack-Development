00f220fe6fd168cabab29c35240431389eb459c0
Sidratul Montaha <sonykhan1121@gmail.com>
2025-03-19 23:13:07 +0600

    use web wrapper print movies list and image from imdb

--- 03 Practice Basic Flutter Projects/test_widget/lib/website_wrapper_features/wrapper_test.dart
+++ 03 Practice Basic Flutter Projects/test_widget/lib/website_wrapper_features/wrapper_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
-import 'package:html/parser.dart' as dom;
 import 'package:http/http.dart' as http;
 
 class WrapperTest extends StatefulWidget {
@@ -11,43 +10,88 @@ class WrapperTest extends StatefulWidget {
 }
 
 class _WrapperTestState extends State<WrapperTest> {
-
+  List<dynamic> list = [];
+  List<dynamic> images_list = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getWebsiteDat();
-
+    getmovieList();
   }
-  Future getWebsiteDat() async
-  {
-    final uri = Uri.parse('https://www.amazon.com/stores/page/D209D922-7883-495C-9894-6B13D9BB1A67/?_encoding=UTF8&ingress=0&visitId=75ce56a0-38d0-4cc3-a911-6b4b8bd24c01&ref_=cct_cg_CEPCLAP_1a1&pf_rd_p=9fb5bb48-7491-4c96-bfef-25750b14b1c4&pf_rd_r=T7MQTAATVWWX2SJSTZ3Q');
+
+  Future getWebsiteDat() async {
+    final uri = Uri.parse('https://fusionbd.com/index.html');
     final response = await http.get(uri);
-    print(response.body);
+    // print(response.body);
 
-    final document = dom.parse(response.body);
-    print(document.body?.text);
-    final titles = document.querySelectorAll('#dossier-asin-grid > div > div.a-section.a-spacing-none.d-asin-grid-content > div.a-section.a-spacing-none.dcl-grid-wrapper > div.a-section.a-spacing-none.dcl-html-grid > span:nth-child(14) > div > div.a-section.a-spacing-base > div > div.a-section.a-spacing-none.a-spacing-top-small.s-title-instructions-style > a')
-    .map((element)=>element.innerHtml.trim())
-    .toList();
-    print("count:${titles.length}");
-    for(final title in titles)
-    {
-      print(title);
-    }
+    final document = parse(response.body);
+
+    // print("Fusionbd :${document.outerHtml}");
+
+    final elements =
+        document
+            .querySelectorAll('body > div > div > b')
+            .map((value) => value.text)
+            .toList();
+    list = elements;
+    setState(() {});
+
+    // print(document.body?.text);
+    // final titles = document.querySelectorAll('#dossier-asin-grid > div > div.a-section.a-spacing-none.d-asin-grid-content > div.a-section.a-spacing-none.dcl-grid-wrapper > div.a-section.a-spacing-none.dcl-html-grid > span:nth-child(14) > div > div.a-section.a-spacing-base > div > div.a-section.a-spacing-none.a-spacing-top-small.s-title-instructions-style > a')
+    // .map((element)=>element.innerHtml.trim())
+    // .toList();
+    // print("count:${titles.length}");
+    // for(final title in titles)
+    // {
+    //   print(title);
+    // }
   }
+
+  Future getmovieList() async {
+    final uri = Uri.parse('https://www.imdb.com/chart/top/');
+    final response = await http.get(uri);
+
+    final document = parse(response.body);
+    final titles =
+        document
+            .querySelectorAll(
+              'div.ipc-title.ipc-title--base.ipc-title--title.ipc-title-link-no-icon.ipc-title--on-textPrimary.sc-3713cfda-2.fSzZES.cli-title.with-margin > a > h3',
+            )
+            .map((value) => value.text)
+            .toList();
+
+    list = titles;
+    final images =
+    document
+        .querySelectorAll(
+      'div > div > div > div > img',
+    )
+        .map((value) => value.attributes['src'])
+        .toList();
+    images_list = images;
+    print(titles);
+    print(images);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Wrapper test'),
-      ),
+      appBar: AppBar(title: Text('Wrapper test')),
       body: Center(
-        child: Column(
-          children: [
-
-          ],
+        child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Column(
+                children: [
+                  Image.network(images_list[index]),
+                  Text(list[index]),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
